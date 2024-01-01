ad3f18cbc51a6edb545fed3ddc25293fee8d176d
yuuki <>
2024-01-02 07:31:53 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ tmux kill-server
 sleep 5
 
 tmux new -s tunnel -d
-tmux send-keys -t tunnel 'cloudflared tunnel --url http://127.0.0.1:8000 --name gorioshi' C-m
+tmux send-keys -t tunnel 'cloudflared tunnel --url http://127.0.0.1:3000/ --name gorioshi' C-m
 
 tmux new -s server -d
 tmux send-keys -t server 'python app.py' C-m
