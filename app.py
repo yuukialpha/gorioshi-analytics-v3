fe6f34bbaed20b9b904bfaec44fd3bf3fff5c1b6
yuuki <>
2024-01-01 17:30:14 +0900

    1日ごとの統計情報も追加

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app = flask.Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 600
 
-@app.route("/<key>/<email>/<zone_id>/<int:hours>")
-def index(key, email, zone_id, hours):
+@app.route("/hourly/<key>/<email>/<zone_id>/<int:hours>")
+def index_hourly(key, email, zone_id, hours):
     transport = gql.transport.aiohttp.AIOHTTPTransport(url="https://api.cloudflare.com/client/v4/graphql", headers={
         "Authorization": f"Bearer {key}",
         "X-Auth-Email": email,
@@ -41,6 +41,50 @@ def index(key, email, zone_id, hours):
 
     fig = matplotlib.figure.Figure()
     fig.suptitle(f"Stats for {zone_id}")
+    fig.set_size_inches(7, 7)
+    ax1, ax2 = fig.subplots(2)
+    ax1.set_title("Users")
+    ax1.plot(ax1_x, ax1_y)
+    ax2.set_title("Bytes")
+    ax2.plot(ax2_x, ax2_y)
+    ax2.plot(ax2_2_x, ax2_2_y)
+
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
+    img = io.BytesIO()
+    canvas.print_png(img)
+    img.seek(0)
+
+    return flask.send_file(img, mimetype="image/png")
+
+@app.route("/daily/<key>/<email>/<zone_id>/<int:days>")
+def index_daily(key, email, zone_id, days):
+    transport = gql.transport.aiohttp.AIOHTTPTransport(url="https://api.cloudflare.com/client/v4/graphql", headers={
+        "Authorization": f"Bearer {key}",
+        "X-Auth-Email": email,
+    })
+    client = gql.Client(transport=transport)
+
+    query_text = pathlib.Path("query2.txt").read_text("UTF-8")
+    query = gql.gql(query_text)
+    params = {
+        "zoneTag": zone_id,
+        "start": (datetime.datetime.now() - datetime.timedelta(days=days)).astimezone(datetime.timezone.utc).date().strftime('%Y-%m-%d'),
+        "end": datetime.datetime.now().astimezone(datetime.timezone.utc).date().strftime('%Y-%m-%d'),
+        "max": days,
+    }
+
+    result = client.execute(query, variable_values=params)
+    first_group = result["viewer"]["zones"][0]["httpRequests1dGroups"]
+
+    ax1_y = [item["uniq"]["uniques"] for item in first_group]
+    ax1_x = range(-len(ax1_y)+1,0+1)
+    ax2_y = [item["sum"]["bytes"] for item in first_group]
+    ax2_x = range(-len(ax2_y)+1,0+1)
+    ax2_2_y = [item["sum"]["cachedBytes"] for item in first_group]
+    ax2_2_x = range(-len(ax2_2_y)+1,0+1)
+
+    fig = matplotlib.figure.Figure()
+    fig.suptitle(f"Stats for {zone_id} (daily)")
     fig.set_size_inches(7, 7)
     ax1, ax2 = fig.subplots(2)
     ax1.set_title("Users")
